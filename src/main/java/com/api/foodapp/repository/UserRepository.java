4131a670b4abd6caa0b3fdff4ee08ab0331e192b
Chibueze <arisachibueze@gmail.com>
2023-06-22 22:52:24 +0100

    FoodApp First commit -- Still on Security config

---
+++ src/main/java/com/api/foodapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.api.foodapp.repository;
+
+import com.api.foodapp.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/api/foodapp/user/Role.java
@@ -0,0 +1,6 @@
+package com.api.foodapp.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
